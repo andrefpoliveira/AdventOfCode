15559c6638d227707f82d633486ce5170e2095ce
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2021-10-28 16:34:19 +0100

    Fixed Day 21 (2015)

--- 2015/day22.py
+++ 2015/day22.py
@@ -24,7 +24,7 @@ def part1(boss_hit, boss_dmg, boss_armor, weapons, armor, rings):
                     min_cost = cost
     return min_cost         
 
-def part2(boss_hit, boss_dmg, boss_armor, weapons, armor, rings):
+def part2():
     max_cost = 0
     for weapon in weapons:
         for arm in armor + [(0,0,0)]:
@@ -53,32 +53,15 @@ if __name__ == "__main__":
         lines = f.readlines()
         boss_hit = int(lines[0].strip().split(" ")[-1])
         boss_dmg = int(lines[1].strip().split(" ")[-1])
-        boss_armor = int(lines[2].strip().split(" ")[-1])
 
-    weapons = [
-        (8, 4, 0),
-        (10, 5, 0),
-        (25, 6, 0),
-        (40, 7, 0),
-        (74, 8, 0)
+    # Mana Cost, Immediate Dmg, Dmg By Turn, Immediate Healing, Armor by Turn, Mana by Turn, Turns
+    spells = [
+        [53, 4, 0, 0, 0, 0, 0],
+        [73, 2, 0, 2, 0, 0, 0],
+        [113, 0, 0, 0, 7, 0, 6],
+        [173, 0, 3, 0, 0, 0, 6],
+        [229, 0, 0, 0, 0, 101, 5]
     ]
 
-    armor = [
-        (13, 0, 1),
-        (31, 0, 2),
-        (53, 0, 3),
-        (75, 0, 4),
-        (102, 0, 5)
-    ]
-
-    rings = [
-        (25, 1, 0),
-        (50, 2, 0),
-        (100, 3, 0),
-        (20, 0, 1),
-        (40, 0, 2),
-        (80, 0, 3),
-    ]
-
-    print(f"Day 21 Part 1: {part1(boss_hit, boss_dmg, boss_armor, weapons, armor, rings)}")
-    print(f"Day 21 Part 2: {part2(boss_hit, boss_dmg, boss_armor, weapons, armor, rings)}")
+    print(f"Day 20 Part 1: {part1(boss_hit, boss_dmg, spells)}")
+    print(f"Day 20 Part 2: {part2()}")
