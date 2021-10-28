d418fdd08325f5b03ba51702c1528267dff3c0b2
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2021-10-28 15:37:09 +0100

    2015 Day 20

---
+++ 2015/day20.py
@@ -0,0 +1,26 @@
+def part1(number):
+    houses = [0 for x in range(number//10)]
+    for elf in range(1, number//10):
+        for house in range(elf, number//10, elf):
+            houses[house-1] += elf * 10
+
+    for i in range(len(houses)):
+        if houses[i] >= number:
+            return i+1
+
+def part2(number):
+    houses = [0 for x in range(number//10)]
+    for elf in range(1, number//10):
+        for house in range(elf, min(number//10, elf*50 + 1), elf):
+            houses[house-1] += elf * 11
+
+    for i in range(len(houses)):
+        if houses[i] >= number:
+            return i+1
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        number = int(f.read().strip())
+
+    print(f"Day 20 Part 1: {part1(number)}")
+    print(f"Day 20 Part 2: {part2(number)}")
