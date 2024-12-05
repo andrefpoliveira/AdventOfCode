f768eed3a3efe28ad492564c3951a2c4e6e0b984
andre.oliveira <andre.oliveira@mollie.com>
2024-12-05 09:05:10 +0000

    01-2024

---
+++ events/2024/day01.py
@@ -0,0 +1,40 @@
+from utils import problem
+from utils import preprocessing as ppr
+
+from collections import Counter
+
+problem = problem.Problem("2024/01: Historian Hysteria")
+problem.preprocessor = ppr.lsv
+
+
+def get_lists(ls):
+    l1, l2 = [], []
+
+    for l in ls:
+        ns = [int(x) for x in l.split()]
+        l1.append(ns[0])
+        l2.append(ns[1])
+
+    l1.sort()
+    l2.sort()
+
+    return l1, l2
+
+
+@problem.solver(part=1)
+def solver(ls):
+    l1, l2 = get_lists(ls)
+    return sum(abs(l1[i] - l2[i]) for i in range(len(l1)))
+
+
+@problem.solver(part=2)
+def solver(ls):
+    l1, l2 = get_lists(ls)
+
+    c1, c2 = Counter(l1), Counter(l2)
+
+    return sum(k * c1[k] * c2.get(k, 0) for k, v in c1.items())
+
+
+if __name__ == "__main__":
+    problem.solve()
